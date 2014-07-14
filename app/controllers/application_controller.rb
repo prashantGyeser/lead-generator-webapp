27934ca839a832e2135e7810eb3746f95243357e
Prashant <prashant@gyeser.com>
2014-07-15 00:50:35 +0530

    Added a redirect so that the user is taken to the dashboard leads page once they sign up or sing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   layout :layout_by_resource
 
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || dashboard_root_path
+  end
+
   protected
 
   def layout_by_resource
