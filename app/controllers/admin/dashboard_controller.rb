ad1c74b4c2a1d723e1e4666d5be20dd92781fbd4
Prashant <prashant@gyeser.com>
2014-07-25 18:35:39 +0530

    Fixed the issue where the data shown in the admin dashboard is wrong becuase I did not subtract a month from the date

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardController < Admin::ApplicationController
   def index
-    @available_leads = Lead.where('date_last_shown < ?', (Time.now - 1)).count
+    @available_leads = Lead.where('date_last_shown < ?', (Time.now - 1.month)).count
     @total_users = User.all.count
   end
 end
