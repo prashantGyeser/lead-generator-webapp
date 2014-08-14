58291445932832c289a1bdd87631d51368e9073e
Prashant <prashant@gyeser.com>
2014-08-14 16:15:58 -0700

    Removed the credit card module for now, need to add it back when the subscription part is done

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
-  def after_sign_up_path_for(resource)
-    'https://urbanzeak.recurly.com/subscribe/coral'
-  end
+  # def after_sign_up_path_for(resource)
+  #   'https://urbanzeak.recurly.com/subscribe/coral'
+  # end
 end
