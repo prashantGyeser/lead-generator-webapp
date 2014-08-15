933a8bedf29f244fbae989be4232c2781d5b4777
Prashant <prashant@gyeser.com>
2014-08-15 03:28:54 -0700

    Fixed the twitter authentication module, still need to push this out and test and see if everything works and then put the search function on a schedule

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,21 +8,10 @@ class SessionsController < ApplicationController
 
     user_id = session[:user_id]
     session[:user_id] = nil
-    #token = Token.from_omniauth(env["omniauth.auth"], user_id)
-
-    puts "The omniauth is: #{env["omniauth.auth"].inspect}"
-
-#    token_hash = AuthHashParser.parse(env["omniauth.auth"])
-
-    puts "The user_id is: #{user_id}"
-
-    #puts "It is using the following token hash to create a token: #{token_hash}"
 
     token = CreateTokenFromAuthHash.create_token(env["omniauth.auth"], user_id)
 
-    puts "It is getting past the create with token: #{token.inspect}"
-
-    #SendMessageToQueue.push_message_to_queue(user_id)
+    SendMessageToQueue.push_message_to_queue(user_id)
 
     redirect_to  dashboard_root_path, notice: 'Successfully integrated Twitter'
 
