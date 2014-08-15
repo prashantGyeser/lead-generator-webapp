6c8559bb75e0a31884012758c9bee296d4698b84
Prashant <prashant@gyeser.com>
2014-08-15 05:51:31 -0700

    Changed the domain url to send the message to, to the live system url

--- lib/send_user_to_external_system.rb
+++ lib/send_user_to_external_system.rb
@@ -5,7 +5,7 @@ class SendUserToExternalSystem
 
   def self.send_user
     params = {'email' => 'prashant@urbanzeak.com', 'oauth_token' => '43453454345dfgbvfdgdf', 'oauth_secret' => '34232dxdkflsdkfdslsfds' }
-    x = Net::HTTP.post_form(URI.parse('http://localhost:4000/users'), params)
+    x = Net::HTTP.post_form(URI.parse('http://urbanzeak-leads-processor.herokuapp.com/users'), params)
     return x
   end
 
