8ef743b8ff0093d86ab5bf53955accaa7d984f33
Prashant <prashant@gyeser.com>
2014-08-16 11:43:16 -0700

    Made it so that a user gets redirected back to the setup page if they have not completed all the 3 steps

--- app/controllers/dashboard/leads_controller.rb
+++ app/controllers/dashboard/leads_controller.rb
@@ -1,14 +1,7 @@
 class Dashboard::LeadsController < Dashboard::ApplicationController
   def index
-
-    current_user_tokens = Token.where(user_id: current_user.id)
-
-    if current_user_tokens.count > 0
-
-    else
+    if SetupStatus.setup_complete?(current_user.id) == false
       redirect_to dashboard_initial_setups_index_path
     end
-
-
   end
 end
