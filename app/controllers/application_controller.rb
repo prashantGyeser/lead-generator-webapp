f2c699c308fae803986c91dec2286ef8979d9419
Prashant <prashant@gyeser.com>
2014-08-18 05:35:58 -0700

    Added a logout button to the dashboard page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,11 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
 end
--- app/controllers/dashboard/application_controller.rb
+++ app/controllers/dashboard/application_controller.rb
@@ -1,9 +1,18 @@
 class Dashboard::ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   before_action :authenticate_user!
 
   layout "dashboard/application"
+
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
 end
