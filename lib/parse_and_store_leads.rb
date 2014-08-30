1a627df336196289344caddc5aed997c9ffb4261
Prashant <prashant@gyeser.com>
2014-08-30 14:11:43 -0700

    Added error handling for the leads import

--- lib/parse_and_store_leads.rb
+++ lib/parse_and_store_leads.rb
@@ -2,15 +2,25 @@ class ParseAndStoreLeads
 
   def self.save_lead(processor_lead)
 
-    user = User.find_by_email(processor_lead["email"])
+    begin
 
-    lead = Lead.new(screen_name: processor_lead["tweet_poster_screen_name"], tweet: processor_lead["tweet_body"], location: processor_lead["user_location"], tweet_id: processor_lead["tweet_id"], user_id: user.id)
+      user = User.find_by_email(processor_lead["email"])
 
-    if lead.save
-      return true
-    else
-      return false
+      lead = Lead.new(screen_name: processor_lead["tweet_poster_screen_name"], tweet: processor_lead["tweet_body"], location: processor_lead["user_location"], tweet_id: processor_lead["tweet_id"], user_id: user.id)
+
+      if lead.save
+        return true
+      else
+        return false
+      end
+    rescue => e
+      Honeybadger.notify(
+          :error_class   => "Import from processor error",
+          :error_message => "Import Error: #{e.message}. The user email is: #{processor_lead["email"]}",
+          :parameters    => processor_lead
+      )
     end
+
 
   end
 
