a42bbd6268449382119a7af8c4d1f550f36106eb
Prashant <prashant@gyeser.com>
2014-09-07 09:19:16 -0700

    Got the basic system up to post message back to the server so that they can be sent to twitter, still need to activate the actual twitter messaging.

--- app/controllers/dashboard/leads_controller.rb
+++ app/controllers/dashboard/leads_controller.rb
@@ -11,7 +11,7 @@ class Dashboard::LeadsController < Dashboard::ApplicationController
   end
 
   def send_reply
-    @tweet_reply = TweetReply.new(tweet_reply_params)
+    #@tweet_reply = TweetReply.new(tweet_reply_params)
 
     token = Token.where(:user_id => current_user.id).last
 
@@ -23,17 +23,17 @@ class Dashboard::LeadsController < Dashboard::ApplicationController
       config.access_token_secret = token.oauth_secret
     end
 
-    lead = Lead.find(params[:tweet_reply][:lead_id])
-    message_to_send = "@#{lead.screen_name}" + params[:tweet_reply][:message]
+    leads_to_message = params[:tweet_reply][:lead_id].split(',')
 
-
-    if @tweet_reply.save
-      flash[:success] = "Message successfully sent"
-      redirect_to dashboard_leads_index_path
-    else
-      flash[:error] = @tweet_reply.errors.empty? ? "Error" : @tweet_reply.errors.full_messages.to_sentence
-      redirect_to dashboard_leads_index_path
+    leads_to_message.each do |lead_to_message|
+      lead = Lead.find(lead_to_message.to_i)
+      message_to_send = "@#{lead.screen_name}" + params[:tweet_reply][:message]
+      TweetReply.create(:message => message_to_send, :lead_id => lead.id, :user_id => current_user.id)
     end
+
+    flash[:success] = "Message successfully sent"
+    redirect_to dashboard_leads_index_path
+
 
   end
 
