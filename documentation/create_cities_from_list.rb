a6a2aca126c71700dc8d7c3d6682f78e0327f1a0
Prashant <prashant@gyeser.com>
2014-09-13 11:23:13 -0700

    Added all the 36 cities and ordered it based on the name in the new lead stream form

--- documentation/create_cities_from_list.rb
+++ documentation/create_cities_from_list.rb
@@ -37,3 +37,12 @@ Toronto
 London
 ).split(/\n/).reject(&:empty?)
 
+cities_array = ["Nashville", "San Diego", "Philadelphia", "Chicago", "Washington DC", "Dallas", "Atlanta", "Los Angeles", "Salt Lake City", "Denver", "Seattle", "Boston", "Austin", "New York", "San Jose", "San Francisco", "Houston", "Phoenix", "San Antonio", "Jacksonville", "Indianapolis", "Columbus", "Charlotte", "Detroit", "Miami", "Memphis", "Oakland", "Las Vegas", "Fort Worth", "Baltimore", "Reno", "Redwood City", "Sparks", "Truckee", "Toronto", "London"]
+
+cities_array.each do |city|
+  cities_exist = City.where(name: city).count
+  if cities_exist > 0
+  else
+    City.create(name: city)
+  end
+end
