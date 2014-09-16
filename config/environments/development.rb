0abbb597be87126dbf290e70dd9857bbb8b9f243
Prashant <prashant@gyeser.com>
2014-09-16 10:13:33 -0700

    Fixed the issue where the icons are not showing up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  config.assets.precompile += %w( .svg .eot .woff .ttf)
+
+
 end
