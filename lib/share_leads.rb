7a943ef91bbc010bb89a072481040bf1cb7a973d
Prashant <prashant@gyeser.com>
2014-09-16 07:04:33 -0700

    Added code to store the lead in a user lead

--- lib/share_leads.rb
+++ lib/share_leads.rb
@@ -5,10 +5,17 @@ class ShareLeads
       if lead.lead_stream_id.blank?
         lead_streams = LeadStream.where(category_id: lead.category_id).where(city_id: lead.city_id)
         lead_streams.each do |lead_stream|
-          user_lead = UserLead.create(lead_id: lead.id, user_id: lead_stream.user_id, lead_stream_id: lead_stream.id)
+          user = User.find(lead_stream.user_id)
+          if ShareLeads.lead_created_after_sign_up?(lead.created_at, user.created_at)
+            user_lead = UserLead.create(lead_id: lead.id, user_id: lead_stream.user_id, lead_stream_id: lead_stream.id)
+          end
         end
       end
     end
+  end
+
+  def self.lead_created_after_sign_up?(lead_date, user_signup_date)
+    return lead_date > user_signup_date
   end
 
 end
