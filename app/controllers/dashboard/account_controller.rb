9c74c51c4e2f7fe546d66dcb0c991c0a823cf88c
Prashant <prashant@gyeser.com>
2014-09-29 03:25:37 -0700

    Added an account page with the links to the respective plans. Still need to test the return and make sure everything works properly

--- app/controllers/dashboard/account_controller.rb
+++ app/controllers/dashboard/account_controller.rb
@@ -1,4 +1,7 @@
 class Dashboard::AccountController < Dashboard::ApplicationController
   def index
+    if Subscription.exists?(user_id: current_user.id)
+      @subscription = Subscription.where(user_id: current_user.id).last
+    end
   end
 end
