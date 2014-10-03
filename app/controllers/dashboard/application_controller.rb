2c725d168647d063abfeac4caf4e467b23f9ed83
Prashant <prashant@gyeser.com>
2014-10-03 03:29:41 -0700

    Added code to disable the app and take the user to the subscriptions page when their trail has ended and when they have not subscribed

--- app/controllers/dashboard/application_controller.rb
+++ app/controllers/dashboard/application_controller.rb
@@ -1,10 +1,14 @@
+require 'subscription_utils'
+
 class Dashboard::ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
-
+  skip_filter :redirect_to_http
   before_action :authenticate_user!
+
+  before_filter :check_subscription
 
   layout "dashboard/application"
 
@@ -14,5 +18,18 @@ class Dashboard::ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
+
+  def check_subscription
+    user = current_user
+
+    subscription_utils = SubscriptionUtils.new
+
+    if subscription_utils.trial_over?(current_user.id) && !subscription_utils.check_if_subscribed?(current_user.id)
+      flash[:message] = "Your trial is over. Please select a plan and subscribe"
+      render 'dashboard/account/index'
+    end
+
+  end
+
 
 end
---
+++ lib/subscription_utils.rb
@@ -0,0 +1,25 @@
+class SubscriptionUtils
+
+  def check_if_subscribed?(user_id)
+    subscription = Subscription.where(user_id: user_id)
+
+    if subscription.blank?
+      subscription_available = false
+    else
+      subscription_available = true
+    end
+    return subscription_available
+  end
+
+  def trial_over?(user_id)
+    user = User.find(user_id)
+
+    if Date.today > (user.created_at + 7)
+      return true
+    end
+
+    return false
+
+  end
+
+end
