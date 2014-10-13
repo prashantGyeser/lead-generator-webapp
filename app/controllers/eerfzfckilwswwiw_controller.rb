d43b5d727ffc049c4fef036efd80e634bfa4986f
Prashant <prashant@gyeser.com>
2014-10-12 22:34:25 -0700

    Added code to show the results for one user

--- app/controllers/eerfzfckilwswwiw_controller.rb
+++ app/controllers/eerfzfckilwswwiw_controller.rb
@@ -1,5 +1,13 @@
 class EerfzfckilwswwiwController < ApplicationController
   def index
-    @user_leads = UserLead.where(:lead_stream_id => 37).order('created_at DESC')
+    user_leads = UserLead.where(:lead_stream_id => 37).order('created_at DESC')
+    @leads = []
+
+    user_leads.each do |user_lead|
+      @leads << Lead.find(user_lead.lead_id)
+    end
+
+    @leads = @leads.uniq {|l| l.screen_name}
+
   end
 end
