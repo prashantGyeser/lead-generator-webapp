fbc4ae82c0748899b917fd03e4957a4505162628
Prashant <prashant@gyeser.com>
2014-10-23 03:17:24 -0700

    Basic layout done. Now need to add angular

--- app/assets/javascripts/admin/users.js
+++ app/assets/javascripts/admin/users.js
@@ -7,6 +7,16 @@ angular.module('Users', [
             {"id": 2, "email": "akhilesh@urbanzeak.com"},
             {"id": 3, "email": "srinivas@urbanzeak.com"},
             {"id": 4, "email": "ron@urbanzeak.com"},
+            {"id": 5, "email": "jules@urbanzeak.com"},
+            {"id": 1, "email": "prashant@urbanzeak.com"},
+            {"id": 2, "email": "akhilesh@urbanzeak.com"},
+            {"id": 3, "email": "srinivas@urbanzeak.com"},
+            {"id": 4, "email": "ron@urbanzeak.com"},
+            {"id": 5, "email": "jules@urbanzeak.com"},
+            {"id": 1, "email": "prashant@urbanzeak.com"},
+            {"id": 2, "email": "akhilesh@urbanzeak.com"},
+            {"id": 3, "email": "srinivas@urbanzeak.com"},
+            {"id": 4, "email": "ron@urbanzeak.com"},
             {"id": 5, "email": "jules@urbanzeak.com"}
         ];
     })
