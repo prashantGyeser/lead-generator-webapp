1f5c5a6339f6d21467a510043c9df0efd0f6e7e2
Prashant <prashant@gyeser.com>
2014-10-23 00:36:55 -0700

    Changed the look and feel of the admin section of the webapp

--- app/assets/javascripts/admin/users.js
+++ app/assets/javascripts/admin/users.js
@@ -1,2 +1,13 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+angular.module('Users', [
+
+])
+.controller('UsersCtrl', function($scope){
+        $scope.users = [
+            {"id": 1, "email": "prashant@urbanzeak.com"},
+            {"id": 2, "email": "akhilesh@urbanzeak.com"},
+            {"id": 3, "email": "srinivas@urbanzeak.com"},
+            {"id": 4, "email": "ron@urbanzeak.com"},
+            {"id": 5, "email": "jules@urbanzeak.com"}
+        ];
+    })
+;
