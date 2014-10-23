363ed6cdd3bc4040bc5687f8d24041dc84f793b9
Prashant <prashant@gyeser.com>
2014-10-23 04:21:36 -0700

    Added code to show the indicators based on if a user is on trial and if they have no leads. Still need to add a function to the users information when the respective list item is clicked

--- app/assets/javascripts/admin/users.js
+++ app/assets/javascripts/admin/users.js
@@ -3,21 +3,21 @@ angular.module('Users', [
 ])
 .controller('UsersCtrl', function($scope){
         $scope.users = [
-            {"id": 1, "email": "prashant@urbanzeak.com"},
-            {"id": 2, "email": "akhilesh@urbanzeak.com"},
-            {"id": 3, "email": "srinivas@urbanzeak.com"},
-            {"id": 4, "email": "ron@urbanzeak.com"},
-            {"id": 5, "email": "jules@urbanzeak.com"},
-            {"id": 1, "email": "prashant@urbanzeak.com"},
-            {"id": 2, "email": "akhilesh@urbanzeak.com"},
-            {"id": 3, "email": "srinivas@urbanzeak.com"},
-            {"id": 4, "email": "ron@urbanzeak.com"},
-            {"id": 5, "email": "jules@urbanzeak.com"},
-            {"id": 1, "email": "prashant@urbanzeak.com"},
-            {"id": 2, "email": "akhilesh@urbanzeak.com"},
-            {"id": 3, "email": "srinivas@urbanzeak.com"},
-            {"id": 4, "email": "ron@urbanzeak.com"},
-            {"id": 5, "email": "jules@urbanzeak.com"}
+            {"id": 1, "email": "prashant@urbanzeak.com", "subscribed": true, "no_leads": true},
+            {"id": 2, "email": "akhilesh@urbanzeak.com", "subscribed": false, "days_remaining": 10, "no_leads": true},
+            {"id": 3, "email": "srinivas@urbanzeak.com", "subscribed": true},
+            {"id": 4, "email": "ron@urbanzeak.com", "subscribed": false, "days_remaining": 10},
+            {"id": 5, "email": "jules@urbanzeak.com", "subscribed": false, "days_remaining": 10},
+            {"id": 1, "email": "prashant@urbanzeak.com", "subscribed": true},
+            {"id": 2, "email": "akhilesh@urbanzeak.com", "subscribed": true},
+            {"id": 3, "email": "srinivas@urbanzeak.com", "subscribed": true},
+            {"id": 4, "email": "ron@urbanzeak.com", "subscribed": true},
+            {"id": 5, "email": "jules@urbanzeak.com", "subscribed": false, "days_remaining": 10},
+            {"id": 1, "email": "prashant@urbanzeak.com", "subscribed": false, "days_remaining": 10},
+            {"id": 2, "email": "akhilesh@urbanzeak.com", "subscribed": false, "days_remaining": 10},
+            {"id": 3, "email": "srinivas@urbanzeak.com", "subscribed": false, "days_remaining": 10},
+            {"id": 4, "email": "ron@urbanzeak.com", "subscribed": false, "days_remaining": 10},
+            {"id": 5, "email": "jules@urbanzeak.com", "subscribed": false, "days_remaining": 10}
         ];
     })
 ;
