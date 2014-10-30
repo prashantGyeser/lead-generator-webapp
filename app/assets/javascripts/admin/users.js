4cd993fae3f29c4f50e396bc3ae809bef834ec94
Prashant <prashant@gyeser.com>
2014-10-30 04:00:09 -0700

    Updated the url to get users and streams from to the productiom url

--- app/assets/javascripts/admin/users.js
+++ app/assets/javascripts/admin/users.js
@@ -3,15 +3,15 @@ angular.module('Users', [
 ])
 .controller('UsersCtrl', function($scope, $http){
 
-        //$http.get("http://www.urbanzeak.com/admin/users/all.json")
-        $http.get("http://localhost:3000/admin/users/all.json")
+        $http.get("http://www.urbanzeak.com/admin/users/all.json")
+        //$http.get("http://localhost:3000/admin/users/all.json")
             .success(function(data){
                 $scope.users = data.users;
                 console.log($scope.users);
             });
 
-        //$http.get("http://www.urbanzeak.com/admin/lead_streams.json")
-        $http.get("http://localhost:3000/admin/lead_streams.json")
+        $http.get("http://www.urbanzeak.com/admin/lead_streams.json")
+        //$http.get("http://localhost:3000/admin/lead_streams.json")
             .success(function(data){
                 $scope.leadStreams = data.leadStreams;
             });
