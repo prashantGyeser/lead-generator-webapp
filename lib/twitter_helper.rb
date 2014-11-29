cba41256ab3a9c18f986cfda2ab7e62eb3887c69
Prashant <prashant@gyeser.com>
2014-11-29 09:10:01 -0800

    Got the basic search working. Before adding code to return the search results to the calling function

--- lib/twitter_helper.rb
+++ lib/twitter_helper.rb
@@ -1,22 +1,31 @@
 class TwitterHelper
 
   def initialize
-    # Client initialization
-    @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['TWITTER_KEY']
-      config.consumer_secret     = ENV['TWITTER_SECRET']
-      config.access_token        = oauth_token.oauth_token
-      config.access_token_secret = oauth_token.oauth_secret
-    end
   end
 
 
   def search(search_term, city)
+    client = initialize_twitter_client
+
+    client.search("to:justinbieber marry me", result_type: "recent").take(3).each do |tweet|
+      puts tweet.text
+    end
 
   end
 
   def reply
 
   end
+
+  private
+  def initialize_twitter_client
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_KEY']
+      config.consumer_secret     = ENV['TWITTER_SECRET']
+      config.access_token        = '2431831016-cQXzpH7eoR7iSBO2A3nb7OLWzNcPBGngAeIxlEz'
+      config.access_token_secret = '2iIxN9FmebV6cHLNZxugz16z3m9qogJIKhc6ubkgHfWM7'
+    end
+  end
+
 
 end
---
+++ spec/lib/twitter_helper_spec.rb
@@ -0,0 +1,14 @@
+require 'twitter_helper'
+require 'rails_helper'
+
+RSpec.describe TwitterHelper do
+
+  it "should return a list of the search results" do
+    twitter_helper = TwitterHelper.new
+
+    results = twitter_helper.search("test","hello")
+
+  end
+
+
+end
