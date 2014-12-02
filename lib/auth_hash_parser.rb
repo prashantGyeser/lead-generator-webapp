a3aa3aac6e10cd44c9b7ce15e6d323ed5dff3de2
Prashant <prashant@gyeser.com>
2014-12-01 23:56:22 -0800

    Fixed the auth hash parser. Before adding code to set the users first lead stream at their root page in case they do not select a lead stream

--- lib/auth_hash_parser.rb
+++ lib/auth_hash_parser.rb
@@ -5,11 +5,10 @@ class AuthHashParser
     token_hash = {}
     token_hash[:provider] = auth_hash[:provider]
     token_hash[:uid] = auth_hash[:uid]
-    puts "This is a test 2: #{auth_hash.inspect}"
     token_hash[:oauth_token] = auth_hash[:credentials][:token]
     token_hash[:oauth_secret] = auth_hash[:credentials][:secret]
     token_hash[:screen_name] = auth_hash[:info][:nickname]
-    #token.user_id = user.id
+
     return token_hash
   end
 
