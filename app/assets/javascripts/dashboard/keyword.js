2be84d80570400186d7cbce6d472e673bc0b40dd
Prashant <prashant@gyeser.com>
2014-12-09 09:47:07 -0800

    Added the aanimation to hide and show the remove and show the input box for a keyword. Still need to wire it up to the backend and show an input box in case the user has entered less than 3 keywords

--- app/assets/javascripts/dashboard/keyword.js
+++ app/assets/javascripts/dashboard/keyword.js
@@ -1,2 +1,15 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function(){
+    $('.keywords a').click(function(event){
+        event.preventDefault();
+        //$(this).parent().prev().empty(10000);
+        var keyword_container = $(this).parent().prev().parent();
+        keyword_container.transition({ opacity: 0 }, function(){
+            keyword_container.empty();
+            keyword_container.append('<td><input type="text"/></td><td><a href="#" class="btn btn_primary">Change Keyword</a></td>');
+        });
+
+        keyword_container.transition({opacity: 1});
+
+
+    });
+});
