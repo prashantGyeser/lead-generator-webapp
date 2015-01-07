24764911a060ede28746a90cd88e2a1f46cf9589
Prashant <prashant@gyeser.com>
2015-01-07 09:00:38 -0800

    Changed the code so that the unprocessed tweets are not deleted instead they are marked as processed

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -33,10 +33,8 @@ class Admin::UsersController < Admin::ApplicationController
 
     respond_to do |format|
       if !results.id.nil?
-        #unprocessed_tweet[:processed] = true
-        #unprocessed_tweet.save
-
-        unprocessed_tweet.destroy
+        unprocessed_tweet[:processed] = true
+        unprocessed_tweet.save
 
         format.json { render :json => results, status: :created }
 
