0fd4700d495a8bfb7f7c3663994ed43791d3a1b1
Prashant <prashant@gyeser.com>
2015-02-27 07:12:24 -0800

    Added the count of the tweets in a day to the app

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ReportsController < Admin::ApplicationController
   def unprocessed_tweets
-    @unprocessed_tweets = UnprocessedTweet.past_year.by_month
+    @unprocessed_tweets_by_day_for_fortnight = UnprocessedTweet.past_year.group_by { |c| c.created_at.to_date }
   end
 end
