1305829ef6c7e9dacf236eaf5cc4ac71a8897d57
Prashant <prashant@gyeser.com>
2015-02-27 07:12:59 -0800

    Changed the code to show data for a day

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ReportsController < Admin::ApplicationController
   def unprocessed_tweets
-    @unprocessed_tweets_by_day_for_fortnight = UnprocessedTweet.past_year.group_by { |c| c.created_at.to_date }
+    @unprocessed_tweets_by_day_for_fortnight = UnprocessedTweet.by_fortnight.group_by { |c| c.created_at.to_date }
   end
 end
