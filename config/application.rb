587e1e2680b87cc70fe3a562e0d4f2bcc01b3541
Prashant <prashant@gyeser.com>
2015-04-02 23:27:54 -0700

    Fixed the mini profiler

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,6 @@ module LeadGeneratorWebapp
     # config.i18n.default_locale = :de
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
-    config.middleware.use Rack::Deflater
 
   end
 end
