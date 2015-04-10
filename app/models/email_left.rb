bcc27f474cd17dad0c4bb858c6cd844726efc306
Prashant <prashant@gyeser.com>
2015-04-10 02:50:13 -0700

    Added code to automatically send out an invite as soon as a user leaves their email

--- app/models/email_left.rb
+++ app/models/email_left.rb
@@ -13,4 +13,10 @@ class EmailLeft < ActiveRecord::Base
 
   validates_uniqueness_of :email
 
+  after_create :send_invite
+
+  def send_invite
+    User.invite!(:email => self.email)
+  end
+
 end
--- app/models/site_email.rb
+++ app/models/site_email.rb
@@ -13,10 +13,4 @@ class SiteEmail < ActiveRecord::Base
 
   validates_uniqueness_of :email
 
-  after_create :send_invite
-
-  def send_invite
-    User.invite!(:email => self.email)
-  end
-
 end
