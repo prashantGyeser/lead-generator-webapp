5b14efd451feadf9f3f6a755658c437a5e126842
Prashant <prashant@gyeser.com>
2015-04-12 08:53:16 -0700

    Added code to send an error message to honeybadger in case there are no results found in a search

--- lib/twitter_helper.rb
+++ lib/twitter_helper.rb
@@ -151,39 +151,49 @@ class TwitterHelper
 
     duplicate_count = 0
 
-    search_results.each do |search_result|
 
-      unprocessed_tweet_hash = {}
-      unprocessed_tweet_hash[:poster_id] = search_result.user.id
-      unprocessed_tweet_hash[:poster_name] = search_result.user.name
-      unprocessed_tweet_hash[:poster_screen_name] = search_result.user.screen_name
-      unprocessed_tweet_hash[:poster_follower_count] = search_result.user.followers_count
-      unprocessed_tweet_hash[:poster_friends_count] = search_result.user.friends_count
-      unprocessed_tweet_hash[:poster_statuses_count] = search_result.user.statuses_count
-      unprocessed_tweet_hash[:tweet_body] = search_result.text
-      unprocessed_tweet_hash[:poster_profile_image_url] = search_result.user.profile_image_url.to_s
-      unprocessed_tweet_hash[:keyword_id] = keyword_id
-      unprocessed_tweet_hash[:tweet_id] = search_result.id.to_s
+    if search_results.count == 0
+      Honeybadger.notify(
+          :error_class   => "No Tweets",
+          :error_message => "No Tweets: #{search_results.inspect}",
+          :parameters    => {keyword_id: keyword_id }
+      )
+    else
+      search_results.each do |search_result|
 
-      unprocessed_tweet = UnprocessedTweet.new(unprocessed_tweet_hash)
+        unprocessed_tweet_hash = {}
+        unprocessed_tweet_hash[:poster_id] = search_result.user.id
+        unprocessed_tweet_hash[:poster_name] = search_result.user.name
+        unprocessed_tweet_hash[:poster_screen_name] = search_result.user.screen_name
+        unprocessed_tweet_hash[:poster_follower_count] = search_result.user.followers_count
+        unprocessed_tweet_hash[:poster_friends_count] = search_result.user.friends_count
+        unprocessed_tweet_hash[:poster_statuses_count] = search_result.user.statuses_count
+        unprocessed_tweet_hash[:tweet_body] = search_result.text
+        unprocessed_tweet_hash[:poster_profile_image_url] = search_result.user.profile_image_url.to_s
+        unprocessed_tweet_hash[:keyword_id] = keyword_id
+        unprocessed_tweet_hash[:tweet_id] = search_result.id.to_s
 
-      begin
-        if unprocessed_tweet.save
+        unprocessed_tweet = UnprocessedTweet.new(unprocessed_tweet_hash)
 
-        else
-          duplicate_count = duplicate_count + 1
+        begin
+          if unprocessed_tweet.save
+
+          else
+            duplicate_count = duplicate_count + 1
+          end
+        rescue => e
+          Honeybadger.notify(
+              :error_class   => "Tweet parse and store error",
+              :error_message => "Tweet parse and store error: #{e.message}",
+              :parameters    => unprocessed_tweet
+          )
         end
-      rescue => e
-        Honeybadger.notify(
-            :error_class   => "Tweet parse and store error",
-            :error_message => "Tweet parse and store error: #{e.message}",
-            :parameters    => unprocessed_tweet
-        )
+
+
+
       end
-
-
-
     end
+
 
     return duplicate_count
 
