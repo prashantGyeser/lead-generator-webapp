c9de9eaac9a6306c86c870136ad820d580b8f959
Prashant <prashant@gyeser.com>
2015-04-13 04:28:18 -0700

    Updated the admin dashboard

--- app/controllers/admin/angular_admin_controller.rb
+++ app/controllers/admin/angular_admin_controller.rb
@@ -88,6 +88,8 @@ class Admin::AngularAdminController < Admin::ApplicationController
 
       keyword_to_return[:email] = user.email
       keyword_to_return[:city] = lead_stream.city_name
+      keyword_to_return[:last_result_count] = lead_stream.last_result_count
+      keyword_to_return[:last_duplicate_count ] = lead_stream.last_duplicate_count
 
       if (subscription_helper.trial_active?(user) || subscription_helper.is_subscribed?(user)) && (keyword.archived == false)
         keyword_to_return[:is_active] = true
