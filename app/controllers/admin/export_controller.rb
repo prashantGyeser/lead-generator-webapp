4a93eed1550dd71f3c3683cb46f1b56f8e41312f
Prashant <prashant@gyeser.com>
2015-04-22 22:25:28 -0700

    Fixed the issue with the export function not working for the admin section

--- app/controllers/admin/export_controller.rb
+++ app/controllers/admin/export_controller.rb
@@ -35,14 +35,28 @@ class Admin::ExportController < Admin::ApplicationController
   private
   def set_keyword_ids
 
+
     user = User.find(params[:user_id])
+
+    puts "The user email is: #{user.email}"
+
     lead_streams = user.lead_streams
+
+    puts "The user lead stream count is: #{lead_streams.count}"
 
     @keyword_ids = []
 
     lead_streams.each do |lead_stream|
-      @keyword_ids + lead_stream.keywords.pluck(:id)
+
+      puts "It is in the lead stream loop"
+
+      @keyword_ids << lead_stream.keywords.pluck(:id)
+
+      puts "It is in the lead stream loop end with keyword ids: #{@keyword_ids}"
+
     end
+
+    @keyword_ids = @keyword_ids.flatten
 
   end
 end
