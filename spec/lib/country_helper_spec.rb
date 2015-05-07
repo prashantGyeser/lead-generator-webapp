3957eb21fc1fd35eef428b196c5ac0bf982fa669
Prashant <prashant@gyeser.com>
2015-05-06 23:29:45 -0700

    Added a rspec test to make sure that even when lower case text compes in the country helper matches it

--- spec/lib/country_helper_spec.rb
+++ spec/lib/country_helper_spec.rb
@@ -15,13 +15,25 @@ RSpec.describe CountryHelper do
 
   end
 
-  it "should return the country id if the location exists" do
+  it "should not return the country id if the location does not exists" do
     country = Country.create(name: "United States")
     location = Location.create(country_id: country.id, name: "New2York")
 
     country_helper = CountryHelper.new
 
     country_details = country_helper.detect_country("New York")
+    expect(country_details[:country_id]).to eq nil
+
+  end
+
+
+  it "should return the country id if the location exists and are in different cases" do
+    country = Country.create(name: "United States")
+    location = Location.create(country_id: country.id, name: "New York")
+
+    country_helper = CountryHelper.new
+
+    country_details = country_helper.detect_country("new york")
     expect(country_details[:country_id]).to eq nil
 
   end
