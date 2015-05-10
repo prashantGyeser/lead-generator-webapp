6ff6460db0fa4841ea02c1d608c4bce56f6aaeed
Prashant <prashant@gyeser.com>
2015-05-09 21:55:44 -0700

    Fixed the issue with a typo in the keyword reports controller

--- app/controllers/admin/keyword_reports_controller.rb
+++ app/controllers/admin/keyword_reports_controller.rb
@@ -15,23 +15,23 @@ class Admin::KeywordReportsController < Admin::ApplicationController
 
     # Leads/(Leads + NonLeads) * 100
     leads = Lead..where(keyword_id: keyword.id).today.count
-    non_leads = NonLead..where(keyword_id: keyword.id).today.count
+    non_leads = NonLead.where(keyword_id: keyword.id).today.count
     @keyword_efficiency << (leads/(leads + non_leads)) * 100
 
     leads = Lead..where(keyword_id: keyword.id).yesterday.count
-    non_leads = NonLead..where(keyword_id: keyword.id).yesterday.count
+    non_leads = NonLead.where(keyword_id: keyword.id).yesterday.count
     @keyword_efficiency << (leads/(leads + non_leads)) * 100
 
     leads = Lead..where(keyword_id: keyword.id).yesterday.count
-    non_leads = NonLead..where(keyword_id: keyword.id).between_times(Time.zone.now - 72.hours,Time.zone.now - 48.hours).count
+    non_leads = NonLead.where(keyword_id: keyword.id).between_times(Time.zone.now - 72.hours,Time.zone.now - 48.hours).count
     @keyword_efficiency << (leads/(leads + non_leads)) * 100
 
     leads = Lead..where(keyword_id: keyword.id).yesterday.count
-    non_leads = NonLead..where(keyword_id: keyword.id).between_times(Time.zone.now - 96.hours,Time.zone.now - 72.hours).count
+    non_leads = NonLead.where(keyword_id: keyword.id).between_times(Time.zone.now - 96.hours,Time.zone.now - 72.hours).count
     @keyword_efficiency << (leads/(leads + non_leads)) * 100
 
     leads = Lead..where(keyword_id: keyword.id).yesterday.count
-    non_leads = NonLead..where(keyword_id: keyword.id).between_times(Time.zone.now - 120.hours,Time.zone.now - 96.hours).count
+    non_leads = NonLead.where(keyword_id: keyword.id).between_times(Time.zone.now - 120.hours,Time.zone.now - 96.hours).count
     @keyword_efficiency << (leads/(leads + non_leads)) * 100
 
   end
